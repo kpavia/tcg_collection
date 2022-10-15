2c9f503a9e0c2353a909eead8885b8398eaf4e34
Kenny <kennethpavia@gmail.com>
2022-10-14 19:29:52 -0600

    test 2

--- core/settings/local.py
+++ core/settings/local.py
@@ -5,3 +5,4 @@ print('=== LOADING local.py ===')
 SECRET_KEY = 'django-insecure-rok@0+ph3u5t7)vb49h=o^(wdp(tz*t^=m8^_mz(%dm#&c#wx3'
 DEBUG = True
 BASE_DIR = Path(__file__).resolve().parent.parent
+print('test')
